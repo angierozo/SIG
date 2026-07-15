c2da44409cff55a9d855323304841e9af2adb067
angierozo <angieromero1040@gmail.com>
2026-07-14 20:47:29 -0500

    Add files via upload

---
+++ programa.py
@@ -0,0 +1,19 @@
+def sumar(a,b):
+    print(a+b)
+    return 50
+print "Hola"
+x=int(input ("ingresa un valor"))
+print x
+
+if x==0:
+    print("x es igual a 0")
+elif x==5:
+    print("x es igual a 5")
+else:
+    print ("x no es 0 ni 5")
+
+while x>0:
+    print (x)
+    x = x - 1
+    
+sumar (5,8)
